b57c9acdfe098b7705fecd8c35e9f45d4322bda4
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-02-20 14:57:30 -0300

    create modal and ErrorBoundary class

---
+++ src/useCurrency.jsx
@@ -0,0 +1,10 @@
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export const priceConverter = (price) => intl.format(price);
+
+export default function useCurrency(price) {
+  return priceConverter(price);
+}
