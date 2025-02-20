94bbddcd5c49eccf78b03fdb258e9215631d5696
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-02-20 16:44:12 -0300

    add default image for pizza and add a test to it

--- src/__tests__/Pizza.test.jsx
+++ src/__tests__/Pizza.test.jsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
 import Pizza from "../Pizza";
+
+afterEach(cleanup);
 
 test("alt test renders on Pizza image", async () => {
   const name = "My favorite Pizza";
@@ -13,4 +15,13 @@ test("alt test renders on Pizza image", async () => {
   const img = screen.getByRole("img");
   expect(img.src).toBe(src);
   expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name="Calabresa Pizza" description="super cool pizza"></Pizza>,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).toBeTruthy();
 });
