9c91d1edc1fb1c123f46d305f5e1367c987b0854
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-02-21 11:17:21 -0300

    install vitest/ui

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,8 @@ export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
   test: {
     environment: "happy-dom",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
   },
 });
